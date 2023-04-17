80a4a729b2178d99cc7c69487a49116d68b5c4dc
Jarno Ralli <jarno@ralli.fi>
2023-04-17 09:08:08 +0300

    Initial commit to get things on the way

---
+++ libstoryboard/include/storyboard/storyboard.hpp
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace storyboard {
+
+class Storyboard;
+class Note;
+
+using tag_cont_t  = std::vector<std::string>;
+using note_cont_t = std::vector<Note>;
+
+/**
+ * @class Note
+ * @brief Simple note-object
+ */
+class Note
+{
+public:
+    /**
+     * @brief Simple constructor
+     */
+    Note() : m_title(""), m_text("")
+    {
+    }
+
+    /**
+     * @brief Constructor with parameters
+     * @param[in] title : Note's title
+     * @param[in] text : Note's text
+     * @param[in] tags : Note's tags
+     */
+    Note(const std::string& title, const std::string& text, tag_cont_t const& tags)
+        : m_title(title), m_text(text), m_tags(tags)
+    {
+    }
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~Note()
+    {
+    }
+
+    /**
+     * @brief Returns a container with the note's tags
+     * @return std-container that contains the note's tags
+     */
+    tag_cont_t& getTags();
+
+    /**
+     * @brief Returns title
+     * @return String containing the note's title
+     */
+    std::string& getTitle();
+
+    /**
+     * @brief Returns text
+     * @return String containing the note's text
+     */
+    std::string& getText();
+
+    /**
+     * @brief operator==
+     * @param[in] other : Compared object
+     * @return true if contents of this object are the same as those of the input object, otherwise return false
+     */
+    bool operator==(const Note& other);
+
+    friend class Storyboard;
+
+protected:
+    std::string m_title;  /// Title of the note-object
+    std::string m_text;   /// Text of the note-object
+    tag_cont_t  m_tags;   /// Tag(s) of the note-object
+};
+
+/**
+ * @class Storyboard
+ * @brief An object that contains Note objects.
+ * @details Has functionality for adding, deleting and searching for Note objects.
+ */
+class Storyboard
+{
+public:
+    /**
+     * @brief Adds a new note object into the Storyboard
+     * @param[in] : newNote note to be added into the Storyboard
+     */
+    void addNote(const Note& newNote);
+
+    /**
+     * @brief Adds a new note object into the Storyboard. Uses move semantics (i.e. steals the resources from the given
+     * object)
+     * @param[in] : newNote note to be added into the Storyboard
+     */
+    void addNote(Note&& newNote);
+
+    /**
+     * @brief Removes a note from the Storyboard
+     * @param[in] deleteMe : Sample of a Note object to be deleted from the Storyboard. Note objects that are equal to
+     * deleteMe will be deleted.
+     * @return number of elements that were deleted
+     */
+    int deleteNote(const Note& deleteMe);
+
+    /**
+     * @brief Search the Storyboard for notes that contain the given string in the title field
+     * @param[in] title : string that is matched
+     * @param[in,out] container : notes in the Storyboard that contain the matched field are added into this container
+     * @return number of search results
+     */
+    int searchByTitle(const std::string& title, note_cont_t& container);
+
+    /**
+     * @brief Search the Storyboard container for notes that contain the given string in the text field
+     * @param[in] text : string that is matched
+     * @param[in,out] container : notes in the Storyboard that contain the matched field are added into this container
+     * @return number of search results
+     */
+    int searchByText(const std::string& text, note_cont_t& container);
+
+    /**
+     * @brief Search the Storyboard container for notes that contain the given string(s) in the tag field
+     * @param[in] tags : a vector of tags that are matched
+     * @param[in,out] container : notes in the Storyboard that contain the matched field are added into this container
+     * @return number of search results
+     */
+    int searchByTag(const tag_cont_t& tags, note_cont_t& container);
+
+    /**
+     * @brief Number of notes stored
+     */
+    int length();
+
+private:
+    note_cont_t m_notes;  /// Container of Note objects
+};
+
+}  // End of namespace storyboard
---
+++ libstoryboard/include/storyboard/storyboardCAPI.h
@@ -0,0 +1,215 @@
+#pragma once
+
+#include "import_export.h"
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// --- Error handling ---
+typedef struct error* error_t_;
+
+/**
+ * @brief
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+const char* error_message(error_t_ error);
+
+/**
+ * @brief
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+error_t_ error_destruct(error_t_ error);
+
+/**
+ * @brief
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+const char* error_get_string(error_t_ error);
+
+// --- Note ---
+typedef struct note* note_t;
+
+/**
+ * @brief Construct a new note
+ * @param[in] title : Title of the note
+ * @param[in] text : Text of the note
+ * @param[in] tags : Tags for the note
+ * @param[in] nr_tags : Number of tags being passed
+ * @param[in, out] out_error : error object
+ * @return a new note object
+ */
+STORYBOARD_EXPORT
+note_t note_construct(const char* title, const char* text, const char* tags[], int32_t nr_tags, error_t_* out_error);
+
+/**
+ * @brief Destructs a note object
+ * @param[in] note_in : a note that is being destructed
+ * @return nullptr
+ */
+STORYBOARD_EXPORT
+note_t note_destruct(note_t note_in);
+
+/**
+ * @brief Copies a note
+ * @param[in] note_in : a note that is copied
+ * @param[in, out] out_error : error object
+ * @return A copy of the note
+ */
+STORYBOARD_EXPORT
+note_t note_copy(const note_t note_in, error_t_* out_error);
+
+/**
+ * @brief Get note's title
+ * @param[in] note_in : note being queried
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+const char* note_get_title(const note_t note_in, error_t_* out_error);
+
+/**
+ * @brief Get note's title
+ * @param[in] note_in : note being queried
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+const char* note_get_text(const note_t note_in, error_t_* out_error);
+
+/**
+ * @brief Note query handler type. Used for querieng note's tags
+ * @param[in] client_data : void pointer to client data object that is passed to the handler
+ * @param[in] tag : Tag of the note
+ */
+typedef void (*note_query_handler)(void* client_data, const char* tag);
+
+/**
+ * @brief Get note's tags
+ * @param[in] note_in : note being queried
+ * @param[in] handler : query handler function
+ * @param[in] client_data : void pointer passed to the handler function
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+void note_get_tags(const note_t note_in, note_query_handler handler, void* client_data, error_t_* out_error);
+
+// --- Storyboard ---
+typedef struct board* board_t;
+
+/**
+ * @brief Constructs a storyboard object
+ * @param[in, out] out_error : error object
+ * @return new board
+ */
+STORYBOARD_EXPORT
+board_t storyboard_construct(error_t_* out_error);
+
+/**
+ * @brief Destructs a storyboard object
+ * @param[in] board_in : board to be destructed
+ * @return nullptr
+ */
+STORYBOARD_EXPORT
+board_t storyboard_destruct(board_t board_in);
+
+/**
+ * @brief Copies a board object
+ * @param[in] board_in : board that is copied
+ * @param[in, out] out_error : error object
+ * @return a copy of the board
+ */
+STORYBOARD_EXPORT
+board_t storyboard_copy(const board_t board_in, error_t_* out_error);
+
+/**
+ * @brief Adds a note to a board
+ * @param[in] board_in : board where the note is added
+ * @param[in] note_in : note that is added to the board
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+void storyboard_add_note(board_t board_in, const note_t note_in, error_t_* out_error);
+
+/**
+ * @brief Deletes a note
+ * @param[in] board_in : board from which the note is deleted
+ * @param[in] note_in : an example of a note that is deleted
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+int32_t storyboard_delete_note(board_t board_in, const note_t note_in, error_t_* out_error);
+
+/**
+ * @brief Storyboard query handler type
+ * @param[in] client_data : void pointer to client data object that is passed to the handler
+ * @param[in] title : Title of the note
+ * @param[in] text : Text of the note
+ * @param[in] tags : Tags of the note
+ * @param[in] nr_tags : Number of tags that the note contains
+ */
+typedef void (*storyboard_query_handler)(void* client_data, const char* title, const char* text, const char* tags[],
+                                         int32_t nr_tags);
+
+/**
+ * @brief Search notes based on the title
+ * @param[in] board_in : board that is being queried
+ * @param[in] name : title/name being queried
+ * @param[in] handler : query handler
+ * @param[in] client_data : client data that is passed to the query handler
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+int32_t storyboard_search_by_title(const board_t board_in, const char* name, storyboard_query_handler handler,
+                                   void* client_data, error_t_* out_error);
+
+/**
+ * @brief Search notes based on the text
+ * @param[in] board_in : board that is being queried
+ * @param[in] text : text being queried
+ * @param[in] handler : query handler
+ * @param[in] client_data : client data that is passed to the query handler
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+int32_t storyboard_search_by_text(const board_t board_in, const char* text, storyboard_query_handler handler,
+                                  void* client_data, error_t_* out_error);
+
+/**
+ * @brief Search notes based on the tag
+ * @param[in] board_in : board that is being queried
+ * @param[in] tag : tag being queried
+ * @param[in] handler : query handler
+ * @param[in] client_data : client data that is passed to the query handler
+ * @param[in, out] out_error : error object
+ * @return
+ */
+STORYBOARD_EXPORT
+int32_t storyboard_search_by_tag(const board_t board_in, const char* tag, storyboard_query_handler handler,
+                                 void* client_data, error_t_* out_error);
+
+/**
+ * @brief Number of notes that the board contains
+ * @param[in] board_in : board that is being queried
+ * @param[in, out] out_error : error object
+ * @return number of notes that the board has
+ */
+STORYBOARD_EXPORT
+int32_t storyboard_get_nr_notes(const board_t board_in, error_t_* out_error);
+
+#ifdef __cplusplus
+}  // End of extern "C"
+#endif
